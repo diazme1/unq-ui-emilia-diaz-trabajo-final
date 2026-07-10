01eb3edd3c98ca470436354f05a86b9f20baf606
emiliadiaz <diaz.m.emilia1@gmail.com>
2026-07-10 20:14:27 -0300

    install y config proyecto root

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Game from './components/Game/Game.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Palabras Encadenadas</h1>
+      ...
+    </div>
+  )
+}
+
+export default App
